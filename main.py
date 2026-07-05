cb8c1294f32adf94cab03c208713b02358bb420e
Mahirul Alam Chowdhury <62341446+Slyyoursenpai@users.noreply.github.com>
2026-07-05 19:35:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from downloader import YTRunner
+
+def main():
+    print("===== Youtube Downloader====")
+    
+    url = input("Paste your Youtube Link here: ").strip()
+    if not url:
+        print("Error: Link was not found.")
+        return
+    
+    print("\n Choose download format:")
+    print("1) 1080p Video\n2) 720p Video\n3) 480p Video\n4) MP3 Audio Only")
+    choice = input("Enter value (1-4): ").strip()
+    
+    success = YTRunner().execute_download(url,choice)
+    
+    if success: 
+        print("\n Success, Cjeck downloaded file")
+    else:
+        print("\n Download failed")
+        
+if __name__ == "__main__":
+    main()
